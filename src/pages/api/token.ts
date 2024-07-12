b31b79b2f07d277c432e484fd7f32f11b6ea11d9
cs-metadexlabs <salvan.cyril.es@gmail.com>
2024-07-13 00:00:11 +0200

    Update token.ts

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -5,8 +5,8 @@ import { AccessToken } from "livekit-server-sdk";
 import type { AccessTokenOptions, VideoGrant } from "livekit-server-sdk";
 import { TokenResult } from "../../lib/types";
 
-const apiKey = process.env.LIVEKIT_API_KEY;
-const apiSecret = process.env.LIVEKIT_API_SECRET;
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const apiSecret = process.env.NEXT_PUBLIC_API_SECRET;
 
 const createToken = (userInfo: AccessTokenOptions, grant: VideoGrant) => {
   const at = new AccessToken(apiKey, apiSecret, userInfo);
